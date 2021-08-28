5a6b1c092c20e3272667b4b4455e15ac2627d8bc
rjcodesandtech <80521857+rjcodesandtech@users.noreply.github.com>
2021-08-28 21:14:40 +0800

    Update py-to-hahaha.py
    Forgot to update one variable name

--- py-to-hahaha.py
+++ py-to-hahaha.py
@@ -6,7 +6,7 @@ fileString = f.read()
 f.close()
 def binaryStringToHAHAHA(binCode):
     toA = binCode.replace("0", "A")
-    toH = toS.replace("1", "H")
+    toH = toA.replace("1", "H")
     return toH
 res = ''.join(format(ord(i), '08b') for i in fileString.upper())
 binaryString = str(res)
